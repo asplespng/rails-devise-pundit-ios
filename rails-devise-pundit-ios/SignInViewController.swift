1aed52a7e967f0debabc79f7a528530202dbf2b9
Per Evensen <per.evensen@gmail.com>
2016-01-03 11:11:37 -0500

    added sign_in view

---
+++ rails-devise-pundit-ios/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.swift
+//  rails-devise-pundit-ios
+//
+//  Created by Per Einar Evensen on 1/3/16.
+//  Copyright © 2016 Per Einar Evensen. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func signIn(sender: AnyObject) {
+        let i = "test"
+    }
+}
